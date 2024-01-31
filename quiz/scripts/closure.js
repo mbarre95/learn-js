23b192b8947bdf09df568beecdd3e838dd09053c
Mo Barre <mbarre95@gmail.com>
2024-01-30 19:05:33 -0500

    Completed 013024 Activities

--- quiz/scripts/closure.js
+++ quiz/scripts/closure.js
@@ -1,6 +1,6 @@
 function setColor(set) {
         let changeColor = set;
-        if(changeColor) {
+        return() => {
             let userColor = document.getElementById('color').value;
             document.getElementById('myPara').style.color = userColor;
         }
